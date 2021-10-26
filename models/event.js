8142bfe0232c25a92312d54edbdd526aa749e41c
Nilotpal Das <neelot3pal@gmail.com>
2021-10-27 02:32:53 +0530

    basic schemas made

---
+++ models/event.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const eventSchema = new mongoose.Schema(
+	{
+	    event_Name: {
+			type: String,
+			required: true
+		},
+        company:{
+            type: String,
+			required: true
+        },
+        category:{
+            type: String,
+			required: true
+        },
+		date: {
+			type: DateTime,
+			required: true
+		},
+		
+
+		participants: [{
+            type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+        }]
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
