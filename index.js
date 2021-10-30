50af6fbc9dfeff67d1daa7cc7a7223cab7e177b8
saif malik <official.saifmalik@gmail.com>
2021-10-30 11:29:29 +0530

    register and login routes implimented

--- index.js
+++ index.js
@@ -1,12 +1,91 @@
 const express = require('express');
-require('./config/mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const {OAuth2Client} = require('google-auth-library');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const user = require('./model/user');
 const app = express();
+const cors = require("cors");
+require('dotenv').config();
 
 const PORT = 4000;
 
-app.get('/',(req,res)=>{
-    res.status(200).json({status:200});
-});
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.post('/login',async(req,res)=>{
+   try {
+      const {emailOrNum,password} = req.body;
+      const User = await user.findOne({emailOrNum});
+      if (User && bcrypt.compareSync(password,User.password)) {
+        const token = jwt.sign(
+          { password },
+          process.env.ACCESS_TOKEN_SIGNATURE , {expiresIn:'1h'}
+        ); 
+        return res.cookie("token",`${token}`).json(User);
+      } else {
+        return res.status(404).json({ code: 404, message: "user not found" });
+      }
+    } catch (error) {
+        res.status(401).json({ code: 401, required: "All Fields are required" });
+    }
+
+})
+
+
+app.post('/signup',async(req,res)=>{
+
+  if(req.body.token){
+      const Id = "913535745495-lut092uh58c03k8b3okk97vkndc0gnei.apps.googleusercontent.com";
+      const client = new OAuth2Client(Id);
+      const ticket = await client.verifyIdToken({idToken:req.body.token,audience:Id});
+      const payload =  ticket.getPayload();
+      if(payload.email_verified){
+        const password = await bcrypt.hash(`${payload.name}`,4); 
+        const User = await user.create({username:payload.name,emailOrNum:payload.email,password,picture:payload.picture});  
+        const token = jwt.sign(
+          { password },
+          process.env.ACCESS_TOKEN_SIGNATURE , {expiresIn:'1h'}
+        ); 
+        return res.cookie("token",`${token}`).json(User);
+      }else{
+        return res.redirected('/login');
+      }
+
+  }else{
+     try {
+    let {username,emailOrNum,password} = req.body; 
+    password = await bcrypt.hash(`${password}`,4); 
+    const User = await user.create({username,emailOrNum,password});  
+     const token = jwt.sign(
+          { password },
+          process.env.ACCESS_TOKEN_SIGNATURE , {expiresIn:'1h'}
+        ); 
+    return res.cookie("token",token).json(User);
+   } catch (error) {
+       console.log(error)
+       res.status(400).json({code:400,required:"All Fields",minPass:6});
+    }
+  }
+
+})
+
+
+app.post('/verify',async(req,res)=>{
+  try{
+     const isverified = await jwt.verify(req.cookies.token,process.env.ACCESS_TOKEN_SIGNATURE);
+     res.status(200).json({status:200});
+  }catch(err){
+     return  res.status(404).json({status:404});
+  }
+})
+
 
 app.listen(PORT,(err)=>{
     if(err)console.log('Server error',err);
