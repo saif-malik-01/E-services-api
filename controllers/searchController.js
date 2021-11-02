b69ebf477a198f2276499e456677c5b9a1daa643
Nilotpal Das <neelot3pal@gmail.com>
2021-11-03 03:41:31 +0530

    search options added in search.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -16,3 +16,77 @@ module.exports.searchAll=async (req,res)=>{
         return res.status(404).json('internal server error');
     }
 }
+
+module.exports.searchAllBooks=async (req,res)=>{
+    try{
+        // const items=await Item.find({});
+        const books=await Ebook.find({});
+        
+        return res.json({
+            // items:items,
+            Ebooks:books
+        });
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(404).json('internal server error');
+    }
+}
+
+
+module.exports.searchAllItems=async (req,res)=>{
+    try{
+        const items=await Item.find({});
+        // const books=await Ebook.find({});
+        
+        return res.json({
+            items:items
+            // Ebooks:books
+        });
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(404).json('internal server error');
+    }
+}
+
+
+module.exports.searchItemCategory= async(req,res)=>{
+    try{
+        const items=await Item.find({
+            category:req.body.type
+        });
+        // const books=await Ebook.find({});
+        
+        return res.json({
+            items:items
+            // Ebooks:books
+        });
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(404).json('internal server error');
+    }
+}
+
+
+module.exports.searchItemCategoryCompany= async (req,res)=>{
+    try{
+        const items=await Item.find({
+            category:req.body.type,
+            company:req.body.company
+        });
+        // const books=await Ebook.find({});
+        
+        return res.json({
+            items:items
+            // Ebooks:books
+        });
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(404).json('internal server error');
+    }
+}
+
+
