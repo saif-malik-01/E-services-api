510e7172767209b826998915684adf35fa7e06e8
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-11-17 14:16:51 +0530

    seperate user routes

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
 	{
-	    Name: {
+	    username: {
 			type: String,
 			required: true
+		},
+
+		email: {
+			type: String,
+			required: true
+		},
+
+		password:{
+			type:String,
+			required:true
 		},
         
 		cartItem: [{
